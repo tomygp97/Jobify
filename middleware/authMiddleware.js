272844f45f73ffb0b6a6e645c49f91fbb70dde7b
tomygp97 <tomasgp1997@gmail.com>
2023-12-19 12:39:40 -0300

    user-routes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import { UnauthenticatedError } from "../errors/customErrors.js";
+import { UnauthenticatedError, UnauthorizedError } from "../errors/customErrors.js";
 import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
@@ -12,4 +12,13 @@ export const authenticateUser = (req, res, next) => {
     } catch (error) {
         throw new UnauthenticatedError('authentication invalid');
     }
+};
+
+export const authorizePermissions = (...roles) => {
+    return (req,res,next) => {
+        if(!roles.includes(req.user.role)){
+            throw new UnauthorizedError('Unauthorized to access this route');
+        };
+        next();
+    };
 };
