447416558c3d52e094cd4e675b26c3f14afe6e71
tomygp97 <tomasgp1997@gmail.com>
2024-09-25 21:49:47 -0300

    Change landing paragraph

--- Client/src/pages/Landing.jsx
+++ Client/src/pages/Landing.jsx
@@ -14,7 +14,8 @@ const Landing = () => {
       <div className="container page">
         <div className="info">
           <h1>job <span>tracking</span> app</h1>
-          <p>Im baby wayfarers hoodie next level taiyaki brooklyn cliche blue bottle single-origin coffee chia. Aesthetic post-ironic venmo, quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch narwhal.</p>
+          {/* <p>Im baby wayfarers hoodie next level taiyaki brooklyn cliche blue bottle single-origin coffee chia. Aesthetic post-ironic venmo, quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch narwhal.</p> */}
+          <p>Effortlessly manage and track job applications with our intuitive app. Stay organized by keeping tabs on application statuses, interview schedules, and follow-ups—all in one place. Whether you are actively job hunting or planning your next career move, streamline your process and never miss an opportunity.</p>
           <Link to='/register' className='btn register-link'>
             Register
           </Link>
